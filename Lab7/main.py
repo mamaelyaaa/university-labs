38633c4645c957e0314471015c68989686245635
mamaelyaaa <poopnp586@gmail.com>
2024-05-14 19:03:27 +0300

    feat: complete Lab7

--- Lab7/main.py
+++ Lab7/main.py
@@ -7,6 +7,9 @@ B: float = 2
 # Погрешность
 E: float = 0.005
 
+# Просчитанный результат
+answer: float = 0.757
+
 
 def f(x: float) -> float:
     return x ** 2 + 1 / atan(x)
@@ -22,7 +25,7 @@ def d2f(x: float) -> float:
 
 def method_golden_section() -> None:
     with open('output.txt', 'w', encoding='utf-8') as output_file:
-        golden_ratio = (1 + 5 ** 0.5) / 2
+        golden_ratio: float = (1 + 5 ** 0.5) / 2
         a, b = A, B
 
         # Выбор начальных точек
@@ -38,26 +41,63 @@ def method_golden_section() -> None:
             x1 = b - (b - a) / golden_ratio
             x2 = a + (b - a) / golden_ratio
 
-        print(f'Метод золотого сечения: {(a + b) / 2:.5f}', file=output_file)
+        print(f'Метод золотого сечения: {(a + b) / 2:.5f}, погрешность: {abs(answer - (a + b) / 2):.5f}',
+              file=output_file)
 
 
 def method_polyline() -> None:
-    pass
+    def delta(l: float, x: float, p: float) -> float:
+        return 1 / (2 * l) * (f(x) - p)
+
+    with open('output.txt', 'a', encoding='utf-8') as output_file:
+        # Находим константу Липшица
+        L = 0
+        h = 0.0001
+
+        for x in range(B, A, -1):
+            L = max(abs((f(x + h) - f(x)) / h), L)
+
+        x: float = 1 / (2 * L) * (f(B) + L * (A + B))
+        p: float = (f(B) + L * (A - B)) / 2
+
+        x1: list = [x - delta(L, x, p), x - delta(L, x, p)]
+        p1: float = (f(x1[0]) + p) / 2
+
+        x_min: float = x1[0] if f(x1[0]) < f(x1[1]) else x1[1]
+
+        while abs(f(x) - f(x_min)) >= E:
+            x1 = [x - delta(x, p, L), x + delta(x, p, L)]
+            p1 = (f(x1[0]) - p) / 2
+
+            x_min = x1[0] if f(x1[0]) < f(x1[1]) else x1[1]
+
+            x = x_min
+            p = p1
+
+        print(f'Метод ломанных:         {x_min:.5f}, погрешность: {abs(answer - x_min):.5f}', file=output_file)
 
 
 def method_tangents() -> None:
     with open('output.txt', 'a', encoding='utf-8') as output_file:
-        x0: float = (A + B) / 2
+        x0 = B  # Если не выдает ошибку, взять другую границу
+        x1 = x0 - f(x0) / df(x0)
 
-        while abs(f(x0)) >= E:
+        while abs(x1 - x0) < E:
             x1 = x0 - f(x0) / df(x0)
             x0 = x1
 
-        print(f'Метод касательных:     {x0:.5f}', file=output_file)
+        print(f'Метод касательных:      {x1:.5f}, погрешность: {abs(answer - x1):.5f}', file=output_file)
 
 
 def method_Newton() -> None:
-    pass
+    with open('output.txt', 'a', encoding='utf-8') as output_file:
+        x0 = B
+
+        while abs(df(x0)) > E:
+            x1 = x0 - (df(x0) / d2f(x0))
+            x0 = x1
+
+        print(f'Метод Ньютона:          {x0:.5f}, погрешность: {abs(answer - x0):.5f}', file=output_file)
 
 
 def main() -> None:
